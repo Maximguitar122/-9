d988b15b5a0af15b40e719abe558d1df97b9e1ec
Maximguitar122 <maxym2251@gmail.com>
2025-08-16 20:22:08 +0300

    Add files via upload

---
+++ ДЗ 9 ООП/ДЗ 9 ООП/ДЗ 9 ООП.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <set>
+#include <string>
+using namespace std;
+
+class Student {
+	string name;
+	int num;
+public:
+	Student(string n, int nu){
+		name = n;
+		num = nu;
+	}
+	string GetName() const {
+		return name;
+	}
+	int GetAge() const {
+		return num;
+	}
+	bool operator <(const Student& other) const {
+		return num < other.num;
+	}
+};
+int main() {
+	set<Student> students;
+	students.insert(Student("Max", 1));
+	students.insert(Student("Dan", 2));
+	students.insert(Student("Lie", 3));
+	cout << "List of Students\n";
+	for (const auto& s : students) {
+		cout << s.GetAge() << "\n";
+		cout << s.GetName() << "\n";
+
+	}
+	Student search("", 3);
+	auto found = students.find(search);
+
+}
